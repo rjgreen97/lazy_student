5fdf5afb24ec7cb9e229f52374ef172257e4bd67
RJ Green <rjgreen97@gmail.com>
2023-12-27 10:16:06 -0600

    button up streamlit interface

--- app.py
+++ app.py
@@ -14,31 +14,40 @@ def find_pdf_files(directory):
             if file.endswith(".pdf"):
                 return file
 
-target_directory = "knowledge_store"
-filename = find_pdf_files(target_directory)
+def home_page():
+    target_directory = "knowledge_store"
+    filename = find_pdf_files(target_directory)
 
-filename_placeholder = st.empty()
+    filename_placeholder = st.empty()
 
-if os.listdir(target_directory):
-    filename_placeholder.write(f"Reference Document: **{filename}**")
-else:
-    filename_placeholder.write("There is currently no document uploaded.")
-
-uploaded_file = st.file_uploader("Upload a file:", type=["pdf"])
-
-if uploaded_file:
     if os.listdir(target_directory):
-        shutil.rmtree(target_directory)
-        os.makedirs(target_directory)
+        filename_placeholder.write(f"Reference Document: **{filename}**")
+    else:
+        filename_placeholder.write("There is currently no document uploaded.")
 
-    file_path = os.path.join(target_directory, uploaded_file.name)
-    with open(file_path, "wb") as f:
-        f.write(uploaded_file.getbuffer())
+    uploaded_file = st.file_uploader("Upload a file:", type=["pdf"])
 
-    filename = uploaded_file.name
-    filename_placeholder.write(f"Reference Document: **{filename}**")
+    if uploaded_file:
+        if os.listdir(target_directory):
+            shutil.rmtree(target_directory)
+            os.makedirs(target_directory)
 
-user_input = st.text_input("Enter your question:")
-if st.button('Ask'):
-    response = response_generator.generate_response(user_input)
-    st.text_area("Answer", value=response, height=300)
+        file_path = os.path.join(target_directory, uploaded_file.name)
+        with open(file_path, "wb") as f:
+            f.write(uploaded_file.getbuffer())
+
+        filename = uploaded_file.name
+        filename_placeholder.write(f"Reference Document: **{filename}**")
+
+    user_input = st.text_input("Enter your question:")
+    if st.button('Ask'):
+        response = response_generator.generate_response(user_input)
+        st.text_area("Answer", value=response, height=300)
+
+
+page_options = {
+    "Home": home_page,
+}
+
+current_page = page_options["Home"]
+current_page()
